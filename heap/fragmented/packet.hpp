58540d7bdd9bf7fa55183b6ff06b2939695d96c8
David C Black <david.black@doulos.com>
2023-06-17 04:45:06 -0500

    Add constructors and to_string
    Features added to improve testing of Pool<T>.
    
    Signed-off-by: David C Black <david.black@doulos.com>

--- heap/fragmented/packet.hpp
+++ heap/fragmented/packet.hpp
@@ -3,12 +3,64 @@
 
 #include <cstdint>
 #include <array>
+#include <string>
 
 template<int size>
 struct Packet {
+  static constexpr const int maxSize = 1u << 20;
+  static_assert( size >= 0 );
+  static_assert( size <= maxSize );
   uint64_t origin{0};
   uint64_t destination{0};
   std::array<uint8_t,size> data{0};
+  std::string to_string();
+  Packet(uint64_t origin, uint64_t dest, std::initializer_list<uint8_t> init )
+  : origin{ origin }, destination{ dest }
+  {
+    int i=0;
+    for( auto v : init ) {
+      if( i >= size ) break;
+      data[i++] = v;
+    }
+  }
+  Packet() = default;
+  Packet( const Packet& pkt ) = default;
+  Packet( Packet&& pkt ) noexcept = default;
+  Packet& operator=( const Packet& pkt ) = default;
+  Packet& operator=( Packet&& pkt ) noexcept = default;
+  ~Packet() noexcept {
+    // Cleanup
+    for( auto& e : data ) e = '\377';
+  }
 };
+
+#include <sstream>
+#include <iomanip>
+
+template<int size>
+std::string Packet<size>::to_string()
+{
+  static std::ostringstream result{};
+  result.str("");
+  result
+    << "{ "
+    << "origin: " << std::to_string(origin) << ", "
+    << "destination: " << std::to_string(destination) << ", "
+    << "size: " << std::to_string(size) << ", "
+    << "data: " << std::hex;
+  for( auto d : data ) {
+    if( ' ' <= d and d <= '~' ) {
+      result << char( d );
+    }
+    else if ( d == 0 ) {
+      result << "\\0";
+    }
+    else {
+      result << "\\x" << int( d );
+    }
+  }
+  result << " }" << std::dec;
+  return result.str();
+}
 
 #endif/*PACKET_HPP*/
