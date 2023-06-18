b2e9d87cd3d4e35aa83da45bb79ad4e983c523ae
David C Black <david.black@doulos.com>
2023-06-17 20:38:18 -0500

    Deeper testing
    Check a variety of conditions.
    
    Signed-off-by: David C Black <david.black@doulos.com>

--- heap/fragmented/pool.cpp
+++ heap/fragmented/pool.cpp
@@ -13,41 +13,53 @@ int main()
     std::cout << "\nInfo: Create pool\n";
     Pool<Packet<5>, 3> pool1{};
     pool1.status( "pool1" );
+    pool1.dump();
 
     // simple allocation
     std::cout << "\nInfo: allocate first packet\n";
-    auto pkt1 = pool1.alloc();
+    auto pkt0 = pool1.alloc();
+    assert( pkt0 != nullptr );
+    std::cout << "pkt0=" << pkt0->to_string() << '\n';
+    pool1.status( "pool1" );
+    pool1.dump();
+
+    std::cout << "\nInfo: allocate second packet using move semantics\n";
+    auto pkt1 = pool1.alloc( Packet<5>{ 1, 2, { 'H', 'e', 'l', 'l', 'o' }} );
     assert( pkt1 != nullptr );
     std::cout << "pkt1=" << pkt1->to_string() << '\n';
     pool1.status( "pool1" );
-
-    std::cout << "\nInfo: allocate second packet using move semantics\n";
-    auto pkt2 = pool1.alloc( Packet<5>{ 1, 2, { 'H', 'e', 'l', 'l', 'o' }} );
-    assert( pkt2 != nullptr );
-    std::cout << "pkt2=" << pkt2->to_string() << '\n';
-    pool1.status( "pool1" );
+    pool1.dump();
 
     std::cout << "\nInfo: allocate third packet\n";
+    auto pkt2 = pool1.alloc(*pkt1);
     std::memcpy( &pkt2->data[0], "World", 5 );
-    auto pkt3 = pool1.alloc(*pkt2);
-    std::cout << "pkt3=" << pkt3->to_string() << '\n';
+    std::cout << "pkt2=" << pkt2->to_string() << '\n';
     pool1.status( "pool1" );
+    pool1.dump();
 
     std::cout << "\nInfo: attempt fourth packet\n";
     auto pkt4 = pool1.alloc();
     assert( pkt4 == nullptr );
 
     std::cout << "\nInfo: free second packet\n";
-    pool1.free( pkt2 );
+    auto pkt1_dup = pkt1;
+    pool1.free( pkt1 );
+    assert( pkt1 == nullptr );
     pool1.status( "pool1" );
+    pool1.dump();
 
     std::cout << "\nInfo: attempt to free a second time second packet\n";
-    pool1.free( pkt2 );
+    pool1.free( pkt1_dup );
     pool1.status( "pool1" );
 
     std::cout << "\nInfo: attempt to free a nullptr\n";
-    pool1.free( nullptr );
+    pool1.free( pkt1 );
     pool1.status( "pool1" );
+
+    std::cout << "\nInfo: clear & free first packet packet\n";
+    pool1.cfree( pkt0 );
+    pool1.status( "pool1" );
+    pool1.dump();
 
     std::cout << "Destroying pool\n";
   }
