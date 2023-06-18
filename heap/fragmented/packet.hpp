5746459239e43e83cfb0441fd3d9416a294c1ee0
David C Black <david.black@doulos.com>
2023-06-18 06:15:32 -0500

    Change constructor/destructor to set a single element.
    Improve performance and avoid worrying about allocated size since size is always at least 1.
    
    Signed-off-by: David C Black <david.black@doulos.com>

--- heap/fragmented/packet.hpp
+++ heap/fragmented/packet.hpp
@@ -4,58 +4,72 @@
 #include <cstdint>
 #include <array>
 #include <string>
+#include <cstring>
 
-template<int size>
+template< int size >
 struct Packet {
   static constexpr const int maxSize = 1u << 20;
   static_assert( size >= 0 );
   static_assert( size <= maxSize );
-  uint64_t origin{0};
-  uint64_t destination{0};
-  std::array<uint8_t,size> data{0};
-  std::string to_string();
-  Packet(uint64_t origin, uint64_t dest, std::initializer_list<uint8_t> init )
-  : origin{ origin }, destination{ dest }
+
+  // Attributes
+  uint64_t origin{ 0 };
+  uint64_t destination{ 1 };
+  std::array<uint8_t, size> data{ 0 };
+
+  // Methods
+  Packet( uint64_t origin, uint64_t dest, std::initializer_list<uint8_t> init )
+          : origin{ origin }, destination{ dest }
   {
-    int i=0;
-    for( auto v : init ) {
-      if( i >= size ) break;
-      data[i++] = v;
+    int i = 0;
+    for ( auto v: init ) {
+      if ( i >= size ) { break; }
+      data[ i++ ] = v;
     }
   }
-  Packet() = default;
+
+  Packet()
+  {
+    data[0] = 'I';
+  }
+
   Packet( const Packet& pkt ) = default;
   Packet( Packet&& pkt ) noexcept = default;
   Packet& operator=( const Packet& pkt ) = default;
   Packet& operator=( Packet&& pkt ) noexcept = default;
-  ~Packet() noexcept {
+
+  ~Packet() noexcept
+  {
     // Cleanup
-    for( auto& e : data ) e = '\377';
+    data[0] = 'D';
   }
+
+  std::string to_string();
+
 };
+
+// Implementation
 
 #include <sstream>
 #include <iomanip>
 
-template<int size>
+template< int size >
 std::string Packet<size>::to_string()
 {
   static std::ostringstream result{};
-  result.str("");
+  result.str( "" );
   result
-    << "{ "
-    << "origin: " << std::to_string(origin) << ", "
-    << "destination: " << std::to_string(destination) << ", "
-    << "size: " << std::to_string(size) << ", "
-    << "data: " << std::hex;
-  for( auto d : data ) {
-    if( ' ' <= d and d <= '~' ) {
+          << "{ "
+          << "origin: " << std::to_string( origin ) << ", "
+          << "destination: " << std::to_string( destination ) << ", "
+          << "size: " << std::to_string( size ) << ", "
+          << "data: " << std::hex;
+  for ( auto d: data ) {
+    if ( ' ' <= d and d <= '~' ) {
       result << char( d );
-    }
-    else if ( d == 0 ) {
+    } else if ( d == 0 ) {
       result << "\\0";
-    }
-    else {
+    } else {
       result << "\\x" << int( d );
     }
   }
